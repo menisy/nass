955008e42b79150564a11b53302bdd04e1157111
menisy <elmenisy@gmail.com>
2015-03-13 02:44:13 +0200

    Fixed top bar and added calendar page design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -41,6 +41,30 @@ module ApplicationHelper
     end
   end
 
+  def week_day(day)
+    days = %w(Sunday Monday Tuesday Wednesday Thursday Friday Saturday)
+    (t days[day].downcase.to_sym).titleize
+  end
+
+  def month_name(month)
+    months = %w(January February March April May June July August September October November December)
+    (t months[month].downcase.to_sym).titleize
+  end
+
+ARABIC_NUMBERS = %w(٠ ١ ٢ ٣ ٤ ٥ ٦ ٧ ٨ ٩)
+def ta numbers
+  numbers = numbers.to_s if numbers.is_a? Integer
+  results = numbers.chars.map { |char| ARABIC_NUMBERS[char.to_i] }.join
+end
+
+def local_number number
+  if I18n.locale.to_s == 'en'
+    number.to_s
+  else
+    ta number
+  end
+end
+
   # def locale_switcher
   #     if I18n.locale.to_s == 'en'
   #       link_to 'عربي', refinery.url_for(params.merge(:locale => 'ar')), class: 'lcl'
